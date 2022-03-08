9126f1aeb327ef89035a140c6118deb5aa66374e
Simon Maltaire <simonmaltaire@gmail.com>
2022-03-09 00:22:30 +0100

    wip
    Signed-off-by: Simon Maltaire <simonmaltaire@gmail.com>

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,20 +1,254 @@
 <template>
-    <div>
-        <v-img height="100vh" cover :src="require(`~/assets/images/image.jpg`)"></v-img>
-    </div>
+    <v-container fluid class="pa-0">
+        <div id="large-header" class="large-header">
+            <canvas id="demo-canvas"></canvas>
+            <v-row align="center" justify="center">
+                <v-btn large rounded class="button-one">Contact us</v-btn>
+                <v-btn large rounded class="button-two">Book a demo</v-btn>
+            </v-row>
+        </div>
+    </v-container>
 </template>
 
 <script>
+import { gsap } from 'gsap';
+import { TweenLite, Circ } from 'gsap/dist/gsap';
+
 export default {
     name: 'IndexPage',
+    mounted() {
+        gsap.registerPlugin(TweenLite, Circ);
+        (function () {
+            let width;
+            let height;
+            let largeHeader;
+            let canvas;
+            let ctx;
+            let points;
+            let target;
+            let animateHeader = true;
+
+            // Main
+            initHeader();
+            initAnimation();
+            addListeners();
+
+            function initHeader() {
+                width = window.innerWidth;
+                height = window.innerHeight;
+                target = { x: width / 2, y: height / 2 };
+
+                largeHeader = document.getElementById('large-header');
+                largeHeader.style.height = height + 'px';
+
+                canvas = document.getElementById('demo-canvas');
+                canvas.width = width;
+                canvas.height = height;
+                ctx = canvas.getContext('2d');
+
+                // create points
+                points = [];
+                for (let x = 0; x < width; x = x + width / 20) {
+                    for (let y = 0; y < height; y = y + height / 20) {
+                        const px = x + (Math.random() * width) / 20;
+                        const py = y + (Math.random() * height) / 20;
+                        const p = { x: px, originX: px, y: py, originY: py };
+                        points.push(p);
+                    }
+                }
+
+                // for each point find the 5 closest points
+                for (let i = 0; i < points.length; i++) {
+                    const closest = [];
+                    const p1 = points[i];
+                    for (let j = 0; j < points.length; j++) {
+                        const p2 = points[j];
+                        if (!(p1 === p2)) {
+                            let placed = false;
+                            for (let k = 0; k < 5; k++) {
+                                if (!placed) {
+                                    if (closest[k] === undefined) {
+                                        closest[k] = p2;
+                                        placed = true;
+                                    }
+                                }
+                            }
+
+                            for (let k = 0; k < 5; k++) {
+                                if (!placed) {
+                                    if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                        closest[k] = p2;
+                                        placed = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    p1.closest = closest;
+                }
+
+                // assign a circle to each point
+                for (const i in points) {
+                    const c = new Circle(points[i], 2 + Math.random() * 2, 'rgba(255,255,255,0.3)');
+                    points[i].circle = c;
+                }
+            }
+
+            // Event handling
+            function addListeners() {
+                if (!('ontouchstart' in window)) {
+                    window.addEventListener('mousemove', mouseMove);
+                }
+                window.addEventListener('scroll', scrollCheck);
+                window.addEventListener('resize', resize);
+            }
+
+            function mouseMove(e) {
+                let posy = 0;
+                let posx = 0;
+                if (e.pageX || e.pageY) {
+                    posx = e.pageX;
+                    posy = e.pageY;
+                } else if (e.clientX || e.clientY) {
+                    posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                    posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+                }
+                target.x = posx;
+                target.y = posy;
+            }
+
+            function scrollCheck() {
+                if (document.body.scrollTop > height) animateHeader = false;
+                else animateHeader = true;
+            }
+
+            function resize() {
+                width = window.innerWidth;
+                height = window.innerHeight;
+                largeHeader.style.height = height + 'px';
+                canvas.width = width;
+                canvas.height = height;
+            }
+
+            // animation
+            function initAnimation() {
+                animate();
+                for (const i in points) {
+                    shiftPoint(points[i]);
+                }
+            }
+
+            function animate() {
+                if (animateHeader) {
+                    ctx.clearRect(0, 0, width, height);
+                    for (const i in points) {
+                        // detect points in range
+                        if (Math.abs(getDistance(target, points[i])) < 4000) {
+                            points[i].active = 0.3;
+                            points[i].circle.active = 0.6;
+                        } else if (Math.abs(getDistance(target, points[i])) < 20000) {
+                            points[i].active = 0.1;
+                            points[i].circle.active = 0.3;
+                        } else if (Math.abs(getDistance(target, points[i])) < 40000) {
+                            points[i].active = 0.02;
+                            points[i].circle.active = 0.1;
+                        } else {
+                            points[i].active = 0;
+                            points[i].circle.active = 0;
+                        }
+
+                        drawLines(points[i]);
+                        points[i].circle.draw();
+                    }
+                }
+                requestAnimationFrame(animate);
+            }
+
+            function shiftPoint(p) {
+                TweenLite.to(p, 1 + 1 * Math.random(), {
+                    x: p.originX - 50 + Math.random() * 100,
+                    y: p.originY - 50 + Math.random() * 100,
+                    ease: Circ.easeInOut,
+                    onComplete() {
+                        shiftPoint(p);
+                    },
+                });
+            }
+
+            // Canvas manipulation
+            function drawLines(p) {
+                if (!p.active) return;
+                for (const i in p.closest) {
+                    ctx.beginPath();
+                    ctx.moveTo(p.x, p.y);
+                    ctx.lineTo(p.closest[i].x, p.closest[i].y);
+                    ctx.strokeStyle = 'rgba(156,217,249,' + p.active + ')';
+                    ctx.stroke();
+                }
+            }
+
+            function Circle(pos, rad, color) {
+                const _this = this;
+
+                // constructor
+                (function () {
+                    _this.pos = pos || null;
+                    _this.radius = rad || null;
+                    _this.color = color || null;
+                })();
+
+                this.draw = function () {
+                    if (!_this.active) return;
+                    ctx.beginPath();
+                    ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+                    ctx.fillStyle = 'rgba(156,217,249,' + _this.active + ')';
+                    ctx.fill();
+                };
+            }
+
+            // Util
+            function getDistance(p1, p2) {
+                return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+            }
+        })();
+    },
 };
 </script>
 
-<style>
-.image {
-    background-image: url('~/assets/images/image.jpg');
+<style lang="scss">
+@import '@/assets/variables.scss';
+.custom-container {
+    padding: 0;
+    margin-top: $app-bar-height;
+}
+.large-header {
+    position: relative;
+    width: 100%;
+    background: #333;
+    overflow: hidden;
+    background-size: cover;
+    background-position: center center;
+}
+
+#large-header {
+    background-image: url('~/assets/images/tech_circle2.jpg');
     background-position: center top;
     background-size: 100% auto;
     height: 100vh;
+}
+
+.button-one {
+    position: absolute;
+    top: 50%;
+    left: 45%;
+    -webkit-transform: translate3d(-50%, -50%, 0);
+    transform: translate3d(-50%, -50%, 0);
+}
+.button-two {
+    position: absolute;
+    top: 50%;
+    left: 55%;
+    -webkit-transform: translate3d(-50%, -50%, 0);
+    transform: translate3d(-50%, -50%, 0);
 }
 </style>
--- client/pages/who_are_we.vue
+++ client/pages/who_are_we.vue
@@ -3,12 +3,3 @@
         <h1>Check out our team members</h1>
     </v-container>
 </template>
-
-<style lang="scss" scoped>
-@import '@/assets/variables.scss';
-.custom-container {
-    padding: 0;
-    margin-top: $app-bar-height;
-    background-color: white;
-}
-</style>
