4ad47532f38e28f59a188ae3bc9b0bb749c30ce1
Simon Maltaire <simonmaltaire@gmail.com>
2022-03-08 14:40:36 +0100

    website WIP
    Signed-off-by: Simon Maltaire <simonmaltaire@gmail.com>

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - client',
-    title: 'client',
+    titleTemplate: '%s - wired beauty',
+    title: 'Wired Beauty',
     htmlAttrs: {
       lang: 'en'
     },
@@ -57,7 +57,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
