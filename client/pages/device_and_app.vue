741a0d606f399260d99c2219e166f20e551efd27
Simon Maltaire <simonmaltaire@gmail.com>
2022-03-09 14:12:18 +0100

    eslint finxing pages
    Signed-off-by: Simon Maltaire <simonmaltaire@gmail.com>

--- client/pages/device_and_app.vue
+++ client/pages/device_and_app.vue
@@ -1,21 +1,20 @@
 <template>
-<div>
-    <v-card color="primary">
-        <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    <p>Lorem ipsum dolor sit amet, consectetur</p>
-    </v-card>
-    
+    <div>
+        <v-card color="primary">
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+        </v-card>
     </div>
 </template>
