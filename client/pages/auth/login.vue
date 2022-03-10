c4ee2c329e293e33d3a6a4adff843935fcf5bfb2
Simon Maltaire <simonmaltaire@gmail.com>
2022-03-10 14:30:56 +0100

    stable
    Signed-off-by: Simon Maltaire <simonmaltaire@gmail.com>

--- client/pages/auth/login.vue
+++ client/pages/auth/login.vue
@@ -1,31 +1,36 @@
 <template>
-    <v-row no-gutters justify="center" align="center" class="img">
-        <v-col cols="6">
-            <v-card color="white" class="pa-3">
-                <v-form ref="form" v-model="valid" lazy-validation>
-                    <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-                    <v-text-field
-                        v-model="password"
-                        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                        :rules="[rules.required, rules.min]"
-                        :type="show ? 'text' : 'password'"
-                        name="input-10-1"
-                        label="Password"
-                        hint="At least 8 characters"
-                        counter
-                        @click:append="show = !show"
-                    ></v-text-field>
-                    <v-btn :loading="loading" :disabled="!valid" color="success" class="mr-4" @click="submit"> Log in </v-btn>
-                </v-form>
-            </v-card>
-        </v-col>
-    </v-row>
+    <div id="login-header" class="large-header">
+        <canvas id="login-canvas"></canvas>
+        <v-row no-gutters justify="center" align="center">
+            <v-col cols="6">
+                <v-card color="white" width="500" class="pa-3 custom-card">
+                    <v-form ref="form" v-model="valid" lazy-validation>
+                        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+                        <v-text-field
+                            v-model="password"
+                            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                            :rules="[rules.required, rules.min]"
+                            :type="show ? 'text' : 'password'"
+                            name="input-10-1"
+                            label="Password"
+                            hint="At least 8 characters"
+                            counter
+                            @click:append="show = !show"
+                        ></v-text-field>
+                        <v-btn :loading="loading" :disabled="!valid" color="success" class="mr-4" @click="submit"> Log in </v-btn>
+                    </v-form>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script>
+import { polygonEffect } from '~/services/polygonEffect';
+
 export default {
     name: 'LoginPage',
-    layout: 'empty',
+    // layout: 'empty',
     data: () => ({
         loader: null,
         valid: true,
@@ -39,6 +44,9 @@ export default {
         },
         show: false,
     }),
+    mounted() {
+        polygonEffect('login-canvas', 'login-header');
+    },
     methods: {
         async submit() {
             this.$refs.form.validate();
@@ -58,7 +66,14 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.custom-card {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate3d(-50%, -50%, 0);
+    transform: translate3d(-50%, -50%, 0);
+}
 .custom-loader {
     animation: loader 1s infinite;
     display: flex;
