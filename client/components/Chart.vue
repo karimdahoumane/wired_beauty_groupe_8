28e0906c53fb637413e69c05463db3df8b3ab1ae
Mehdi Saber <m1d3s@protonmail.com>
2022-03-11 01:21:26 +0100

    WIP Chart in admin page

---
+++ client/components/Chart.vue
@@ -0,0 +1,19 @@
+<template>
+  <canvas :id="id"></canvas>
+</template>
+
+<script>
+import Chart from "chart.js/auto";
+export default {
+  props: {
+    data: { type: Object || Array, required: true },
+    id: {type: String, required: true}
+  },
+
+  async mounted() {
+    const ctx = document.getElementById(this.id).getContext('2d');
+    const myChart = new Chart(ctx, this.data);
+  }
+
+}
+</script>
--- client/pages/admin/index.vue
+++ client/pages/admin/index.vue
@@ -1,5 +1,13 @@
 <template>
-    <p>BRAVO TU ES CONNECTED LOL</p>
+  <v-container fluid>
+    <v-card class="dataviz">
+      <v-card-title>Activity report</v-card-title>
+      <v-card-text>Here are your activity report results for your product : Exquisite Cream</v-card-text>
+      <Chart :data="config" id="dataviz"/>
+    </v-card>
+
+  </v-container>
+
 </template>
 
 <script>
@@ -7,7 +15,30 @@ export default {
     name: 'IndexAdminPage',
     layout: 'admin',
     middleware: 'auth',
+  data: () => ({
+    config: {
+      type: 'line',
+      data: {
+        labels: ["T0", "T1", "T2", "T3", "T4", "T5", "T6"],
+        datasets: [{
+          label: 'Mesures sur peau non traitée',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        },
+          {
+            label: 'Mesures sur peau traitée',
+            data: [100, 53, 110, 89, 75, 50, 35],
+            fill: false,
+            borderColor: 'rgb(200, 192, 192)',
+            tension: 0.1
+          }]
+      },
+    }
+  }),
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
