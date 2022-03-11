381fdb0d6d952ff42c33fb0d148e644cb15e26bd
Mehdi Saber <m1d3s@protonmail.com>
2022-03-11 11:38:17 +0100

    Upload file done

--- api/app/Http/Controllers/UploadController.php
+++ api/app/Http/Controllers/UploadController.php
@@ -2,25 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Imports\BaseImport;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Excel;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
+use Throwable;
 
 class UploadController extends Controller
 {
     public function __invoke(Request $request) {
-        try {
-            $path = Storage::disk('local')->put(Carbon::now()->timestamp(), $request->file('excel-import'));
-            Excel::import(new BaseImport, $path);
-            return response()->json([
-                "statusCode" => 200,
-                "message" => "File successfully uploaded & imported"
-            ]);
-        } catch (\Throwable $throwable) {
-            return response(status: 500)->json([
-                "statusCode" => 500,
-                "message" => $throwable->getMessage()
-            ]);
-        }
+            $uploadedFile = $request->file('excel');
+            $fileName = Carbon::now()->timestamp . '.' . $uploadedFile->getClientOriginalExtension() ;
+            if (Storage::disk('local')->put($fileName, $uploadedFile->getContent())) {
+                Excel::import(new BaseImport, $fileName, 'local');
+                return [
+                    "statusCode" => 200,
+                    "message" => "File successfully uploaded & imported"
+                ];
+            }
+
     }
 }
