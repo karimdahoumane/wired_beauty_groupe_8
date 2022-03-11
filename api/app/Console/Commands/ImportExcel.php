56fb833c0fe1fb2ae0fcf276fd5380147c3c2edb
Your Karim DAHOUMANE <krmdahoumane@gmail.com>
2022-03-11 15:19:58 +0100

    Implemented all the graphics

--- api/app/Console/Commands/ImportExcel.php
+++ api/app/Console/Commands/ImportExcel.php
@@ -40,62 +40,6 @@ class ImportExcel extends Command
      */
     public function handle()
     {
-        $moyenne1T0 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 1)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',1)
-            ->avg('mesure');
-        $moyenne1T1 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 1)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',2)
-            ->avg('mesure');
-        $moyenne1T2 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 1)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',3)
-            ->avg('mesure');
-        $moyenne1T3 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 1)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',4)
-            ->avg('mesure');
-
-        $moyenne2T0 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 2)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',1)
-            ->avg('mesure');
-        $moyenne2T1 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 2)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',2)
-            ->avg('mesure');
-        $moyenne2T2 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 2)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',3)
-            ->avg('mesure');
-        $moyenne2T3 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 2)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',4)
-            ->avg('mesure');
-
-        dump('Valeurs delta de l\'efficacité du produit SKC (appliqué et non appliqué) sur 4 temps');
-        dump('T0 : '.$moyenne2T0 - $moyenne1T0);
-        dump('T1 : '.$moyenne2T1 - $moyenne1T1);
-        dump('T8 : '.$moyenne2T2 - $moyenne1T2);
-        dump('T15 : '.$moyenne2T3 - $moyenne1T3);
-        //Excel::import(new BaseImport, 'DatasHackaton_2022_08_03.xlsx');
 
         return 0;
     }
--- api/app/Http/Controllers/GraphController.php
+++ api/app/Http/Controllers/GraphController.php
@@ -12,82 +12,141 @@ class GraphController extends Controller
 
 
         $products = Rapport::query()->select('product_code')->distinct()->get()->toArray();
+        $zones = Rapport::query()->select('zone_code')->distinct()->get()->toArray();
+        $scores = Rapport::query()->select('score_skinbiosense')->distinct()->get()->toArray();
+        $sessions = Rapport::query()->select('session_id')->distinct()->get()->toArray();
+        $data = array();
 
-        //SKC appliqué
-        $moyenne1T0 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 1)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',1)
-            ->avg('mesure');
-        $moyenne1T1 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 1)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',2)
-            ->avg('mesure');
-        $moyenne1T2 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 1)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',3)
-            ->avg('mesure');
-        $moyenne1T3 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 1)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',4)
-            ->avg('mesure');
+        foreach ($products as $product) {
+            foreach ($scores as $score) {
+                foreach ($zones as $zone) {
+                    foreach ($sessions as $session) {
+                        array_push($data, Rapport::query()
+                            ->where('product_code', $product)
+                            ->where('zone_code', $zone)
+                            ->where('score_skinbiosense',$score)
+                            ->where('session_id',$session)
+                            ->avg('mesure'));
+                    }
+                }
+            }
+        }
 
-        //SKC non appliqué
-        $moyenne2T0 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 2)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',1)
-            ->avg('mesure');
-        $moyenne2T1 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 2)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',2)
-            ->avg('mesure');
-        $moyenne2T2 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 2)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',3)
-            ->avg('mesure');
-        $moyenne2T3 = Rapport::query()
-            ->where('product_code', 417432)
-            ->where('zone_code', 2)
-            ->where('score_skinbiosense',1)
-            ->where('session_id',4)
-            ->avg('mesure');
-
-
-        $data1 = [$moyenne1T0, $moyenne1T1, $moyenne1T2, $moyenne1T3];
-        $data2 = [$moyenne2T0, $moyenne2T1, $moyenne2T2, $moyenne2T3];
-
-
-        return response()->json([
-            'type' => 'line',
-            'data' => [
-                'labels' => ['T0', 'T1', 'T8', 'T15'],
-                'datasets' => [[
-                    'label' => 'Produit appliqué',
-                    'data' => $data1,
-                    'fill' => false,
-                    'borderColor' => 'rgb(75, 192, 192)',
-                    'tension' => 0.1
-                ],[
-                    'label' => 'Produit non appliqué',
-                    'data' => $data2,
-                    'fill' => false,
-                    'borderColor' => 'rgb(34, 100, 192)',
-                    'tension' => 0.1
-                ]]
-            ]
+        return response()->json(
+            [
+                [
+                'type' => 'line',
+                'data' => [
+                    'labels' => ['T0', 'T1', 'T8', 'T15'],
+                    'datasets' => [[
+                        'label' => 'Produit appliqué',
+                        'data' => [$data[0], $data[1], $data[2], $data[3]],
+                        'fill' => false,
+                        'borderColor' => 'rgb(75, 192, 192)',
+                        'tension' => 0.1
+                    ],[
+                        'label' => 'Produit non appliqué',
+                        'data' => [$data[4], $data[5], $data[6], $data[7]],
+                        'fill' => false,
+                        'borderColor' => 'rgb(34, 100, 192)',
+                        'tension' => 0.1
+                    ]]
+                ]],
+                [
+                    'type' => 'line',
+                    'data' => [
+                        'labels' => ['T0', 'T2', 'T8', 'T15'],
+                        'datasets' => [[
+                            'label' => 'Produit appliqué',
+                            'data' => [$data[8], $data[9], $data[10], $data[11]],
+                            'fill' => false,
+                            'borderColor' => 'rgb(75, 192, 192)',
+                            'tension' => 0.1
+                        ],[
+                            'label' => 'Produit non appliqué',
+                            'data' => [$data[12], $data[13], $data[14], $data[15]],
+                            'fill' => false,
+                            'borderColor' => 'rgb(34, 100, 192)',
+                            'tension' => 0.1
+                        ]]
+                    ]
+                ],
+                [
+                    'type' => 'line',
+                    'data' => [
+                        'labels' => ['T0', 'T2', 'T8', 'T15'],
+                        'datasets' => [[
+                            'label' => 'Produit appliqué',
+                            'data' => [$data[16], $data[17], $data[18], $data[19]],
+                            'fill' => false,
+                            'borderColor' => 'rgb(75, 192, 192)',
+                            'tension' => 0.1
+                        ],[
+                            'label' => 'Produit non appliqué',
+                            'data' => [$data[20], $data[21], $data[22], $data[23]],
+                            'fill' => false,
+                            'borderColor' => 'rgb(34, 100, 192)',
+                            'tension' => 0.1
+                        ]]
+                    ]
+                ],
+                [
+                    'type' => 'line',
+                    'data' => [
+                        'labels' => ['T0', 'T2', 'T8', 'T15'],
+                        'datasets' => [[
+                            'label' => 'Produit appliqué',
+                            'data' => [$data[24], $data[25], $data[26], $data[27]],
+                            'fill' => false,
+                            'borderColor' => 'rgb(75, 192, 192)',
+                            'tension' => 0.1
+                        ],[
+                            'label' => 'Produit non appliqué',
+                            'data' => [$data[28], $data[29], $data[30], $data[31]],
+                            'fill' => false,
+                            'borderColor' => 'rgb(34, 100, 192)',
+                            'tension' => 0.1
+                        ]]
+                    ]
+                ],
+                [
+                    'type' => 'line',
+                    'data' => [
+                        'labels' => ['T0', 'T2', 'T8', 'T15'],
+                        'datasets' => [[
+                            'label' => 'Produit appliqué',
+                            'data' => [$data[32], $data[33], $data[34], $data[35]],
+                            'fill' => false,
+                            'borderColor' => 'rgb(75, 192, 192)',
+                            'tension' => 0.1
+                        ],[
+                            'label' => 'Produit non appliqué',
+                            'data' => [$data[36], $data[37], $data[38], $data[39]],
+                            'fill' => false,
+                            'borderColor' => 'rgb(34, 100, 192)',
+                            'tension' => 0.1
+                        ]]
+                    ]
+                ],
+                [
+                    'type' => 'line',
+                    'data' => [
+                        'labels' => ['T0', 'T2', 'T8', 'T15'],
+                        'datasets' => [[
+                            'label' => 'Produit appliqué',
+                            'data' => [$data[40], $data[41], $data[42], $data[43]],
+                            'fill' => false,
+                            'borderColor' => 'rgb(75, 192, 192)',
+                            'tension' => 0.1
+                        ],[
+                            'label' => 'Produit non appliqué',
+                            'data' => [$data[44], $data[45], $data[46], $data[47]],
+                            'fill' => false,
+                            'borderColor' => 'rgb(34, 100, 192)',
+                            'tension' => 0.1
+                        ]]
+                    ]
+                ]
         ]);
     }
 }
