935b58be92b91b7603da1674aaf6097bbdb48d92
Simon Maltaire <simonmaltaire@gmail.com>
2022-03-12 15:55:23 +0100

    footer ok
    Signed-off-by: Simon Maltaire <simonmaltaire@gmail.com>

---
+++ client/components/AppFooter.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-footer class="pa-0 ma-0 bg-black" height="180">
+        <v-row no-gutters align="center" justify="center">
+            <v-col cols="10">
+                <p class="mb-0 white--text">WIRED BEAUTY</p>
+                <v-divider class="my-7"></v-divider>
+                <v-row align="center" justify="space-bewteen">
+                    <v-col>
+                        <p class="mb-0 white--text">Newsletter</p>
+                    </v-col>
+                    <v-col>
+                        <p class="mb-0 white--text">Contact</p>
+                    </v-col>
+                    <v-col>
+                        <p class="mb-0 white--text">Follow us</p>
+                    </v-col>
+                    <v-col>
+                        <p class="mb-0 white--text">Legal & Privacy</p>
+                    </v-col>
+                    <v-col>
+                        <p class="mb-0 white--text">Cookies</p>
+                    </v-col>
+                    <v-col>
+                        <p class="mb-0 white--text">Jobs</p>
+                    </v-col>
+                    <v-col>
+                        <p class="mb-0 white--text">Sitemap</p>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-footer>
+</template>
+
+<style scoped>
+.bg-black {
+    background-color: #121111;
+}
+.theme--light.v-divider {
+    border-color: #292626 !important;
+}
+</style>
