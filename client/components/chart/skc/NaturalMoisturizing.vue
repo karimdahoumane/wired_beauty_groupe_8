812c2bfd1cd85827e13f0e50ac49f913de7a1f83
Simon Maltaire <simonmaltaire@gmail.com>
2022-03-12 14:48:28 +0100

    csv download chart ok
    Signed-off-by: Simon Maltaire <simonmaltaire@gmail.com>

--- client/components/chart/skc/NaturalMoisturizing.vue
+++ client/components/chart/skc/NaturalMoisturizing.vue
@@ -12,6 +12,10 @@
                                 <v-list-item-title>.PNG</v-list-item-title>
                                 <v-btn icon @click="downloadChartAsPNG(chart)"><v-icon>mdi-image</v-icon></v-btn>
                             </v-list-item>
+                            <v-list-item>
+                                <v-list-item-title>.CSV</v-list-item-title>
+                                <v-btn icon @click="downloadChartAsCSV(chart)"><v-icon>mdi-file-delimited</v-icon></v-btn>
+                            </v-list-item>
                         </v-list>
                     </v-menu>
                 </v-row>
@@ -22,7 +26,7 @@
 </template>
 
 <script>
-import { createChart, downloadChartAsPNG } from '@/services/chart.services';
+import { createChart, downloadChartAsPNG, downloadChartAsCSV } from '@/services/chart.services';
 import { naturalMoisturizingGraphConfig } from '@/chart/skc/naturalMoisturizingGraph';
 export default {
     name: 'SkcNaturalMoisturizing',
@@ -36,6 +40,7 @@ export default {
     },
     methods: {
         downloadChartAsPNG,
+        downloadChartAsCSV,
     },
 };
 </script>
--- client/services/chart.services.js
+++ client/services/chart.services.js
@@ -14,3 +14,51 @@ export const downloadChartAsPNG = (chart) => {
     a.download = filename;
     a.click();
 };
+
+export const downloadChartAsCSV = (chart) => {
+    const config = chart.config._config;
+    const datasets = config.data.datasets;
+    const labels = config.data.labels;
+    const title = config.data.title;
+    let csv = '';
+    // csv header
+    csv += `${title || 'Undefined Column'},`;
+    for (const dataset of datasets) {
+        const label = dataset.label
+            .replaceAll(',', '')
+            .normalize('NFD')
+            .replace(/[\u0300-\u036F]/g, '');
+        csv += label + ',';
+    }
+    csv = csv.slice(0, -1) + '\n';
+
+    // csv body
+    for (const i in datasets[0].data) {
+        // data
+        const label = labels[i]
+            .replaceAll(',', '')
+            .normalize('NFD')
+            .replace(/[\u0300-\u036F]/g, '');
+        csv += label + ',';
+
+        for (const j in datasets) {
+            // data
+            csv += datasets[j].data[i] + ',';
+        }
+
+        csv = csv.slice(0, -1) + '\n';
+    }
+
+    if (!csv) return;
+
+    const filename = config.filename ? config.filename + '.csv' : 'chart.csv';
+    csv = 'data:text/csv;charset=utf-8,' + csv;
+
+    const data = encodeURI(csv);
+    const link = document.createElement('a');
+    link.setAttribute('href', data);
+    link.setAttribute('download', filename);
+    document.body.append(link);
+    link.click();
+    document.body.removeChild(link);
+};
